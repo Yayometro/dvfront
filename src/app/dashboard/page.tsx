0c4ddc0593e2abe025b321246693275826f4a04a
Jair Vazquez <gsfrido10@outlook.com>
2024-07-25 17:24:20 -0600

    middleware for nexthAuth was removed and session handled per route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
-function Dashboard() {
+// import DashboardClient from '@/components/dashboard/DashboardClient'
+
+
+async function Dashboard() {
+    const session = await getServerSession()
+    if (!session) {
+      console.log("session", session)
+        console.log('No session on Dashboard')
+        redirect("/login")
+    }
+        
   return (
     <div className='general-dashboard-container w-full h-full lg:px-4 lg:pt-6'>
       {/* <DashboardClient /> */}
